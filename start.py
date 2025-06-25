0ef62427433fb3e5b627db9b358721ca1f83408c
nikzzxiter <nikzzxiter@users.noreply.github.com>
2025-06-25 14:29:21 +0700

    Add Dockerfile for Railway deployment

---
+++ start.py
@@ -0,0 +1,7 @@
+from FIXEDENCODE_READY import main
+import nest_asyncio
+import asyncio
+
+if __name__ == "__main__":
+    nest_asyncio.apply()
+    asyncio.get_event_loop().run_until_complete(main())
