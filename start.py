1653f36c68b9b642688be1e1410fd87d5884603f
nikzzxiter <nikzzxiter@users.noreply.github.com>
2025-06-25 12:34:31 +0700

    initial push

---
+++ start.py
@@ -0,0 +1,17 @@
+import asyncio
+from telegram.ext import ApplicationBuilder
+import os
+from FIXEDENCODE import CommandHandler, Update, ContextTypes
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot aktif dari Railway 🚀")
+
+async def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
